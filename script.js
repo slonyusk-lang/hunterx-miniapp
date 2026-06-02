6466a8dab50a1b8505dcb63100e9552ad34e304a
slonyusk-lang <slonyusk@gmail.com>
2026-06-02 22:39:41 +0630

    Update script.js

--- script.js
+++ script.js
@@ -23,13 +23,20 @@ function withdraw() {
 let level = 1;
 
 function checkLevel() {
-  if (points >= 1000) {
-    level = 2;
-  }
 
-  if (points >= 5000) {
-    level = 3;
-  }
+  if (points >= 1000) level = 2;
+  if (points >= 5000) level = 3;
+  if (points >= 10000) level = 4;
+  if (points >= 20000) level = 5;
+  if (points >= 50000) level = 6;
+  if (points >= 100000) level = 7;
+  if (points >= 250000) level = 8;
+  if (points >= 500000) level = 9;
+  if (points >= 1000000) level = 10;
+
+  document.getElementById("level").innerText =
+    "Level " + level;
+}
 
   document.getElementById("level").innerText =
     "Level " + level;
